14e9cf010cf076ca479b821c16e62e173fed047d
lsalvatierra <luisangel.salvat@gmail.com>
2025-07-21 18:55:09 -0500

    agregar ejemplos

--- scripts/nodos-queryselector.js
+++ scripts/nodos-queryselector.js
@@ -1,5 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let mensaje = prompt("Ingrese mensaje:")
-    let parrafoMensaje = document.querySelector("#mensaje");
-    parrafoMensaje.textContent = mensaje;
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let mensaje = prompt("Ingrese mensaje:");
+  let parrafoMensaje = document.querySelector("#mensaje");
+  parrafoMensaje.textContent = mensaje;
+
+  let bloques = document.querySelectorAll(".bloque");
+  bloques.forEach(function (bloque, index) {
+    if (index % 2 === 0) {
+      // Solo los bloques en posición par
+      bloque.style.border = "2px solid black";
+    }
+  });
+});
